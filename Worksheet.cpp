46afe95d4fedb22d3842a301560168a251d9291d
Dimitar Mihaylov <dimitar.mihaylov@mytum.de>
2019-09-24 14:02:02 +0200

    Worksheet added

---
+++ Worksheet.cpp
@@ -0,0 +1,30 @@
+
+#include "Worksheet.h"
+#include "Basics.h"
+#include "CATS.h"
+#include "CATStools.h"
+#include "CATSconstants.h"
+#include "TGraph.h"
+#include "TString.h"
+#include "TF1.h"
+#include "TFile.h"
+#include "TH1F.h"
+
+void Worksheet_ProtonLambda(){
+
+    const unsigned NumMomBins = 60;
+    const double kMin = 0;
+    const double kMax = 240;
+
+    CATSparameters POT_PARS_1S0(CATSparameters::tPotential,4,true);
+    POT_PARS_1S0.SetParameter(0,0);
+    POT_PARS_1S0.SetParameter(1,2137);
+    POT_PARS_1S0.SetParameter(2,0.5);
+    POT_PARS_1S0.SetParameter(3,0.2);
+    CATSparameters POT_PARS_3S1(CATSparameters::tPotential,4,true);
+    POT_PARS_3S1.SetParameter(0,1);
+    POT_PARS_3S1.SetParameter(1,2137);
+    POT_PARS_3S1.SetParameter(2,0.5);
+    POT_PARS_3S1.SetParameter(3,0.2);
+
+}
